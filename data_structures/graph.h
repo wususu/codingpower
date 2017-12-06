6c8ba613f7b317f2ef7204eee600afe1d230dc04
janke <keven_susu@126.com>
2017-12-06 11:38:27 +0800

    Add some remove method

--- data_structures/graph.h
+++ data_structures/graph.h
@@ -28,53 +28,25 @@ public:
 
     Vertex(int number, K name, T weight):Vertex(number, name, weight, NULL){};
 
-    Vertex(int number,K name, T weight, Vertex<T, K>* next)
-    {
-        this->number = number;
-        this->name = name;
-        this->weight = weight;
-        this->next = next;
-    }
+    Vertex(int number,K name, T weight, Vertex<T, K>* next);
 
-    void setW(int weight)
-    {
-        this->weight = weight;
-    }
+    void setW(int weight);
 
-    Vertex<T, K>* getNext()
-    {
-        return this->next;
-    }
+    Vertex<T, K>* getNext();
 
-    K getName()
-    {
-        return this->name;
-    }
+    K getName();
 
-    T getW()
-    {
-        return this->weight;
-    }
+    T getW();
 
-    int getN()
-    {
-        return this->number;
-    }
+    int getN();
 
-    void setNext(Vertex<T, K> *next)
-    {
-        this->next = next;
-    }
-
-
+    void setNext(Vertex<T, K> *next);
 };
-
-
 
 /**
  * 顶点类
  * */
-template <typename T, typename K>
+template<typename T, typename K>
 class Vroot
 {
 private:
@@ -87,61 +59,207 @@ private:
     int arcN = 0;
     // 链表头
     Vertex<T, K> *root;
+
     Vroot<T, K> *next;
 
 public:
+
     enum {error = 0, ok = 1};
 
     Vroot():Vroot(0, NULL, NULL){};
 
     Vroot(int number, K name):Vroot(number, NULL, name){};
 
-    Vroot(int number, Vertex<T, K>* root, K name)
-    {
-        this->number = number;
-        this->root = root;
-        this->name = name;
-        this->next = NULL;
-    }
+    Vroot(int number, Vertex<T, K>* root, K name);
 
-    void setName(K name)
-    {
-        this->name = name;
-    }
+    ~Vroot();
 
-    K getName()
-    {
-        return this->name;
-    }
+    void setName(K name);
 
-    void setNext(Vroot *next)
-    {
-        this->next = next;
-    }
+    K getName();
 
-    Vroot<T, K> * getNext()
-    {
-        return this->next;
-    }
+    void setNext(Vroot *next);
 
-    int getN()
-    {
-        return this->number;
-    }
+    Vroot<T, K> * getNext();
 
-    Vertex<T, K>* getVertexRoot()
-    {
-        return this->root;
-    }
+    int getN();
+
+    void setVertexRoot(Vertex<T, K> *v);
+
+    Vertex<T, K>* getVertexRoot();
 
     bool addVertex(int head, K name, T weight);
+
     bool contain(int head);
 
+    // 移除该顶点指向第head个顶点的弧
+    bool remove(int head);
 };
 
 
 
+/**
+ * 图
+ * */
+template <typename T, typename K>
+class Graph
+{
 
+private:
+
+Vroot<T, K> *vhead;
+// 当前顶点数
+int num = 0;
+
+public:
+    enum {error = 0, ok = 1};
+
+    Graph();
+
+    ~Graph();
+    
+    int getN();
+    
+    void incN();
+
+    void decN();
+    
+    bool contains(int name);
+    
+    Vroot<T, K>* getVroot(int n);
+    
+    bool addVroot(Vroot<T, K>* vroot);
+    
+    int insert(K name);
+    /**
+     * 移除第num个顶点及所有指向该点的弧
+     **/
+    
+    bool remove(int num);
+    
+    bool change(int num, K name);
+    
+    /**
+     * 给编号为bottom与head的顶点添加一条权重为weight的有向边
+     * */
+    bool addArc(int bottom, int head, T weight);
+    
+    bool removeArc(int bottom, int head);
+    
+    //debug
+    void printVroots();
+};
+
+template <typename T, typename K>
+Vertex<T, K>::Vertex(int number,K name, T weight, Vertex<T, K>* next)
+{
+    this->number = number;
+    this->name = name;
+    this->weight = weight;
+    this->next = next;
+}
+
+template <typename T, typename K>
+void Vertex<T, K>::setW(int weight)
+{
+    this->weight = weight;
+}
+
+template <typename T, typename K>
+Vertex<T, K>* Vertex<T, K>::getNext()
+{
+    return this->next;
+}
+
+template<typename T, typename K>
+K Vertex<T, K>::getName()
+{
+    return this->name;
+}
+
+template<typename T, typename K>
+T Vertex<T, K>::getW()
+{
+    return this->weight;
+}
+
+template<typename T, typename K>
+int Vertex<T, K>::getN()
+{
+    return this->number;
+}
+
+template<typename T, typename K>
+void Vertex<T, K>::setNext(Vertex<T, K> *next)
+{
+    this->next = next;
+}
+
+
+
+template<typename T, typename K>
+Vroot<T, K>::Vroot(int number, Vertex<T, K>* root, K name)
+{
+    this->number = number;
+    this->root = root;
+    this->name = name;
+    this->next = NULL;
+}
+
+template <typename T, typename K>
+Vroot<T, K>::~Vroot()
+{
+    Vertex<T, K> *p = this->root;
+    if(p != NULL)
+    {
+        p = p->getNext();
+        delete this->root;
+        this->root = p;
+    }
+    delete p, this->root;
+}
+
+template<typename T, typename K>
+void Vroot<T, K>::setName(K name)
+{
+    this->name = name;
+}
+
+template <typename T, typename K>
+K Vroot<T, K>::getName()
+{
+    return this->name;
+}
+
+template <typename T, typename K>
+void Vroot<T, K>::setNext(Vroot *next)
+{
+    this->next = next;
+}
+
+template <typename T, typename K>
+Vroot<T, K>* Vroot<T ,K>::getNext()
+{
+    return this->next;
+}
+
+template <typename T, typename K>
+int Vroot<T, K>::getN()
+{
+    return this->number;
+}
+
+template <typename T, typename K>
+void Vroot<T, K>::setVertexRoot(Vertex<T, K> *v)
+{
+    this->root = v;
+}
+
+template <typename T, typename K>
+Vertex<T, K>* Vroot<T, K>::getVertexRoot()
+{
+    return this->root;
+}
 
 /**
  * 添加头顶点的记录结点
@@ -156,11 +274,11 @@ bool Vroot<T, K>::addVertex(int head, K name, T weight)
     if(p == NULL)
     {
         this->root = v;
-        return ok;
+        return true;
     }
     if(this->contain(head))
     {
-        return error;
+        return false;
     }
     else
     {
@@ -170,8 +288,9 @@ bool Vroot<T, K>::addVertex(int head, K name, T weight)
             p = p->getNext();
         }
         p->setNext(v);
-        return ok;
+        return true;
     }
+    return false;
 }
 
 template <typename T, typename K>
@@ -189,46 +308,47 @@ bool Vroot<T, K>::contain(int head)
     return error;
 }
 
-
-
-/**
- * 图
- * */
 template <typename T, typename K>
-class Graph
+bool Vroot<T, K>::remove(int head)
 {
-private:
-Vroot<T, K> *vhead;
-// 当前顶点数
-int num = 0;
-
-public:
-    enum {error = 0, ok = 1};
-
-    Graph();
-    ~Graph();
-    int getN();
-    void incN();
-    bool contains(K name);
-    Vroot<T, K>* getVroot(int n);
-    int addVroot(K weight);
-    bool insert(Vroot<T, K>* vroot);
-    bool remove(int num);
-    bool change(int num, K name);
-    /**
-     * 给编号为bottom与head的顶点添加一条权重为weight的有向边
-     * */
-    bool addArc(int bottom, int head, T weight);
-
-    //debug
-    void printVroots();
-};
-
+    Vertex<T, K> *v = this->getVertexRoot();
+    Vertex<T, K> *w = v->getNext();
+    if(v->getN() == head)
+    {
+        this->setVertexRoot(v->getNext());
+    }
+    while(w != NULL)
+    {
+        if(w->getN() == head)
+        {
+            v->setNext(w->getNext());
+            delete w;
+            w = v->getNext();            
+            continue;
+        }
+        v = w;
+        w = v->getNext();
+    }
+    return true;
+}
 
 template <typename T, typename K>
 Graph<T, K>::Graph()
 {
     this->vhead = NULL;
+}
+
+template <typename T, typename K>
+Graph<T, K>::~Graph()
+{
+    Vroot<T, K> *p = this->vhead;
+    while(p != NULL)
+    {
+        p = p->getNext();
+        delete this->vhead;
+        this->vhead = p;
+    }
+    delete p, this->vhead;
 }
 
 template <typename T, typename K>
@@ -244,30 +364,38 @@ void Graph<T, K>::incN()
 }
 
 template <typename T, typename K>
-bool Graph<T, K>::insert(Vroot<T, K>* vroot)
+void Graph<T, K>::decN()
 {
-    vroot->setNext(NULL);
-    Vroot<T, K> *p = this->vhead;
-    if(p == NULL)
+    if(this->num > 0)
     {
-        this->vhead = vroot;
-        return true;
+        this->num--;
     }
-    while(p->getNext() != NULL)
+}
+
+/**
+ * 插入新的顶点,成功返回该点的序号码,否则返回0
+ **/
+template <typename T, typename K>
+int Graph<T, K>::insert(K name)
+{
+
+    this->incN();
+    Vroot<T, K> *vRoot = new Vroot<T, K>(this->getN(), name);
+
+    if(this->addVroot(vRoot))
     {
-        p = p->getNext();
+        return this->getN();
     }
-    p->setNext(vroot);
-    return true;
+    return error;
 }
 
 template <typename T, typename K>
-bool Graph<T, K>::contains(K name)
+bool Graph<T, K>::contains(int num)
 {
     Vroot<T, K> *p = this->vhead;
     while(p != NULL)
     {
-        if(p->getName() == name)
+        if(p->getN() == num)
         {
             return true;
         }
@@ -288,6 +416,42 @@ bool Graph<T, K>::change(int number, K name)
     return true;
 }
 
+template <typename T, typename K>
+bool Graph<T, K>::remove(int num)
+{
+    if(num<1 || num>this->num)
+    {
+        return error;
+    }
+    if(!this->contains(num))
+    {
+        return error;
+    }
+    Vroot<T, K> *p = this->vhead;
+    Vroot<T, K> *q = p->getNext();
+    if(p->getN() == num)
+    {
+        this->vhead = p->getNext();
+    }
+    p->remove(num);
+    while(q!=NULL)
+    {
+        if(q->getN() == num)
+        {
+            this->decN();
+            p->setNext(q->getNext());
+            delete q;
+            q = p->getNext();
+            continue;
+        }
+        q->remove(num);
+
+        p = q;
+        q = p->getNext();
+    }
+    return true;
+}
+
 /**
  * 获取第n(1~n)个顶点 
  **/
@@ -298,7 +462,7 @@ Vroot<T, K>* Graph<T, K>::getVroot(int n)
     {
         int i=1;
         Vroot<T, K> *p = this->vhead;
-       while(i<n && p->getNext()!=NULL)
+        while(i<n && p->getNext()!=NULL)
        {
            p = p->getNext();
            i++;
@@ -308,28 +472,22 @@ Vroot<T, K>* Graph<T, K>::getVroot(int n)
     return NULL;
 }
 
-/**
- * 插入新的顶点,成功返回该点的序号码,否则返回0
- **/
 template <typename T, typename K>
-int Graph<T, K>::addVroot(K name)
+bool Graph<T, K>::addVroot(Vroot<T, K> *vroot)
 {
-
-    if(this->contains(name))
+    vroot->setNext(NULL);
+    Vroot<T, K> *p = this->vhead;
+    if(p == NULL)
     {
-        return error;
+        this->vhead = vroot;
+        return true;
     }
-
-    this->incN();
-    Vroot<T, K> *vRoot = new Vroot<T, K>(this->getN(), name);
-
-    if(this->insert(vRoot))
+    while(p->getNext() != NULL)
     {
-
-        return this->getN();
-        
+        p = p->getNext();
     }
-    return error;
+    p->setNext(vroot);
+    return true;
 }
 
 template <typename T, typename K>
@@ -345,6 +503,14 @@ bool Graph<T, K>::addArc(int bottom, int head, T weight)
 }
 
 template <typename T, typename K>
+bool Graph<T, K>::removeArc(int bottom, int head)
+{
+    if(bottom <1 || bottom > this->num || head < 1 || head > this->num)
+        return false;
+    return this->getVroot(bottom)->remove(head);
+}
+
+template <typename T, typename K>
 void Graph<T, K>::printVroots()
 {
     Vroot<T, K> *p = this->vhead;
@@ -354,12 +520,10 @@ void Graph<T, K>::printVroots()
         Vertex<T, K> *v = p->getVertexRoot();
         while(v!=NULL)
         {
-            std::cout<<p->getName()<<"->"<<v->getName()<<" 权值："<<v->getW()<<std::endl;
+            std::cout<<p->getN()<<"("<<p->getName()<<")->"<<v->getN()<<"("<<v->getName()<<") 权值："<<v->getW()<<std::endl;
             v = v->getNext();
         }
         p = p->getNext();
     }
 }
-
-
 }
--- data_structures/graphtest.cpp
+++ data_structures/graphtest.cpp
@@ -6,13 +6,11 @@ int main()
     gv::Graph<int, int> *graph = new gv::Graph<int ,int>();
         std::cout<<"in1"<<std::endl;
 
-    int a= graph->addVroot(3);
+    int a= graph->insert(3);
 
-    int b = graph->addVroot(4);
+    int b = graph->insert(4);
 
-    int c = graph->addVroot(11);
-    std::cout<<"in2"<<std::endl;
-
+    int c = graph->insert(11);
 
     graph->change(c, 12);
 
@@ -26,6 +24,14 @@ int main()
     graph->addArc(c, a, 3);
 
 
+    graph->printVroots();
+
+
+    graph->removeArc(1,3);
+    graph->remove(b);
+
+
+    std::cout<<"------"<<std::endl;
     graph->printVroots();
     return 0;
 }
